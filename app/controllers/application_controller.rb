b2a0614d5adfe25b487dd46becaaeca957f92a68
yoppy-luppy <ysk11knst@gmail.com>
2020-08-10 14:28:36 +0900

    単体テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters,if: :devise_controller?
-  #  before_action :authenticate_user!
-
-
-  #  def after_sign_out_path_for
-  #   root_path
-  #  end
-  #  private
-
+  
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth])
    end
