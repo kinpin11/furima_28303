ca37b2f0645ea0212bb4b91dbe563f7a42166534
yoppy-luppy <ysk11knst@gmail.com>
2020-08-12 22:32:48 +0900

    fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show, :search]
   def index
-  end
+    @item = Item.find(1)
+   end
 
   def new
     @item = Item.new
@@ -16,15 +17,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = @item.includes(:user)
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :payer_id, :shipping_origin_id, :date_until_shipping_id, :price, :user_id).merge(user_id: current_user.id)
   end
-
-  # def move_to_index
-  #   unless user_signed_in?
-  #     redirect_to action: :index
-  #   end
-  # end
 end
