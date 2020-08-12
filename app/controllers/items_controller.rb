e37fd4285c85f6247ce92b0aa0f0259a8ed490eb
yoppy-luppy <ysk11knst@gmail.com>
2020-08-12 16:43:52 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,32 +3,28 @@ class ItemsController < ApplicationController
   def index
   end
 
-
   def new
     @item = Item.new
   end
-  
 
   def create
     @item = Item.new(item_params)
-     if @item.save
+    if @item.save
       redirect_to root_path
-     else
-       render :new
-     end
-    
+    else
+      render :new
+    end
   end
 
- private
+  private
 
   def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :payer_id, :shipping_origin_id, :date_until_shipping_id, :price, :user_id ).merge(user_id: current_user.id)
-
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :payer_id, :shipping_origin_id, :date_until_shipping_id, :price, :user_id).merge(user_id: current_user.id)
   end
 
   # def move_to_index
   #   unless user_signed_in?
-  #     redirect_to action: :index 
+  #     redirect_to action: :index
   #   end
   # end
 end
--- app/models/date_until_shipping.rb
+++ app/models/date_until_shipping.rb
@@ -3,8 +3,6 @@ class DateUntilShipping < ActiveHash::Base
     { id: 1, name: '1〜２日で発送' },
     { id: 2, name: '２〜３日で発送' },
     { id: 3, name: '4〜７日で発送' }
-  
-]
+
+  ]
 end
-
-
