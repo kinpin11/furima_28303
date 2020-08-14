6c919fbf04268c4b5c8574f98b6ce595aa94f653
yoppy-luppy <ysk11knst@gmail.com>
2020-08-14 15:54:34 +0900

    エラーバンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,8 +17,12 @@ class ItemsController < ApplicationController
     end
   end
   def update
-    @item.update(item_params)
-    redirect_to root_path
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      
+      render :edit
+    end  
   end
 
   def edit
