acc9391ee0f1af2f8f49ce630cfd9eb069243df1
yoppy-luppy <ysk11knst@gmail.com>
2020-08-14 16:49:53 +0900

    onlyをexceptに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
