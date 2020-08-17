db2a42c3819c5de61869a59d5a1125c4eccb173f
yoppy-luppy <ysk11knst@gmail.com>
2020-08-17 20:49:52 +0900

    単体テストコード

--- app/forms/user_buyer.rb
+++ app/forms/user_buyer.rb
@@ -1,13 +1,14 @@
 class UserBuyer
 
   include ActiveModel::Model
+
   attr_accessor :post_code, :shipping_origin_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
 
-  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :shipping_origin_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :city,   format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "can't be blank" }
-  validates :house_number,   format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "can't be blank" }
-  validates :phone_number,   format: {with: /\A[a-zA-Z0-9]+\z/, message: "can't be blank" }
+  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :shipping_origin_id, numericality: { other_than: 0}  
+  validates :city,   presence: true
+  validates :house_number,  presence: true
+  validates :phone_number,   format: {with: /\A[a-zA-Z0-9]+\z/}
 
   def save
     Buyer.create(user_id: user_id, item_id: item_id)
@@ -17,3 +18,4 @@ class UserBuyer
   end
 
 end
+  
---
+++ spec/models/user_buyer_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe UserBuyer, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_buyer = FactoryBot.build(:user_buyer)
+  end
+
+  describe '送付先住所を保存' do
+    context "商品が購入できる時場合" do
+      
+      it "配送先の住所情報が正しい時購入できる" do
+        
+        expect(@user_buyer).to be_valid
+      end
+    end
+    
+    context "商品が購入できない場合" do
+     
+      it "郵便番号が空だと保存できない" do
+        @user_buyer.post_code = ""
+        @user_buyer.valid?
+        expect(@user_buyer.errors.full_messages).to include("Post code is invalid") 
+    
+      end   
+      
+      it "郵便番号に、- がないと保存できない" do
+        @user_buyer.post_code = "1234567"
+        @user_buyer.valid?
+        expect(@user_buyer.errors.full_messages).to include("Post code is invalid") 
+    
+      end    
+       
+      it "都道府県が空だと保存できない" do
+        @user_buyer.shipping_origin_id = ""
+        @user_buyer.valid?
+        expect(@user_buyer.errors.full_messages).to include( "Shipping origin is not a number" )
+      end
+      it "市区町村が空だと保存できない" do
+        @user_buyer.city = ""
+        @user_buyer.valid?
+  
+        expect(@user_buyer.errors.full_messages).to include("City can't be blank") 
+      
+      end
+      it "番地が空だと保存できない" do
+        @user_buyer.house_number = ""
+
+        @user_buyer.valid?
+       
+        expect(@user_buyer.errors.full_messages).to include("House number can't be blank") 
+      end
+      it "電話番号が空だと保存できない" do
+        @user_buyer.phone_number = ""
+        @user_buyer.valid?
+        expect(@user_buyer.errors.full_messages).to include("Phone number is invalid") 
+      end
+
+    end
+
+
+
+  end
+
+end
