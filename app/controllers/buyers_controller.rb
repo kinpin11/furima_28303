936893074f3ec1a7a575838dab07b6c2fba6608d
yoppy-luppy <ysk11knst@gmail.com>
2020-08-19 15:37:37 +0900

    購入機能修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,6 +1,6 @@
 class BuyersController < ApplicationController
   before_action :set_buyer, only: [:index, :create]
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, only: [:index]
 
   def index
     @user_buyer = UserBuyer.new
@@ -38,6 +38,8 @@ class BuyersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    unless user_signed_in?
+    redirect_to root_path
+    end
   end
 end
